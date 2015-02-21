6f090a440739444a94964a2769b1cfdf1cadc1e8
JamesClonk <jamesclonk@jamesclonk.ch>
2015-02-21 17:12:48 +0100

    nginx reverse proxying for shipyard working

--- provision.sh
+++ provision.sh
@@ -8,6 +8,7 @@ source includes.sh
 # error handling
 trap "error \"Error!\"; exit" INT TERM EXIT
 
+
 # read configuration
 header "Read configuration from .env"
 export $(cat .env | xargs)
@@ -21,10 +22,12 @@ export MARS_CERTS="${MARS}_certificates"
 export PHOBOS_CERTS="${PHOBOS}_certificates"
 export DEIMOS_CERTS="${DEIMOS}_certificates"
 
+
 # create certs for docker hosts
 ./cert-gen.sh ${MARS}
 ./cert-gen.sh ${PHOBOS}
 ./cert-gen.sh ${DEIMOS}
+
 
 # provision droplets
 header "Provision DigitalOcean droplets"
@@ -33,6 +36,7 @@ echo "${PHOBOS} nyc3 512mb docker" >> droplets_to_provision.dat
 echo "${DEIMOS} ams3 512mb docker" >> droplets_to_provision.dat
 # TODO: uncomment line below!
 #go run droplets.go droplets_to_provision.dat
+
 
 # wait for ssh
 header "Waiting for SSH"
@@ -47,21 +51,27 @@ for ip in ${IP_ADDRESSES[@]}; do
 	done
 done
 
+
 # update /etc/hosts
 # TODO: add entries to /etc/hosts.. (but only if they dont exist there yet)
+
 
 # update dnsimple entries
 # TODO: modify DNSimple..
 
+
 # update machines
 header "Update virtual machines"
-parallel -v --linebuffer ssh root@{1} "apt-get -q -y update" ::: ${MARS_IP} ${PHOBOS_IP} ${DEIMOS_IP}
-parallel -v --linebuffer ssh root@{1} "apt-get -q -y upgrade" ::: ${MARS_IP} ${PHOBOS_IP} ${DEIMOS_IP}
+# TODO: uncomment the lines below
+#parallel -v --linebuffer ssh root@{1} "apt-get -q -y update" ::: ${MARS_IP} ${PHOBOS_IP} ${DEIMOS_IP}
+#parallel -v --linebuffer ssh root@{1} "apt-get -q -y upgrade" ::: ${MARS_IP} ${PHOBOS_IP} ${DEIMOS_IP}
+
 
 # firewall setup
 header "Setup firewall (ufw)"
 parallel -v --linebuffer scp -r etc/default/ufw root@{1}:/etc/default/. ::: ${MARS_IP} ${PHOBOS_IP} ${DEIMOS_IP}
 # ~~TODO~~: ufw reload (not needed, ufw is not active by default on DO docker image)
+
 
 # upload certs to docker hosts
 header "Upload certificates"
@@ -71,11 +81,13 @@ parallel -v --linebuffer scp -o StrictHostKeyChecking=no -r {1} root@${MARS_IP}:
 scp -r ${PHOBOS_CERTS} root@${PHOBOS_IP}:.
 scp -r ${DEIMOS_CERTS} root@${DEIMOS_IP}:.
 
+
 # setup docker
 header "Setup docker"
 # shutdown/kill all containers
-parallel -v --linebuffer ssh root@{1} 'docker ps -a -q | xargs docker kill' ::: ${MARS_IP} ${PHOBOS_IP} ${DEIMOS_IP}
-parallel -v --linebuffer ssh root@{1} 'docker ps -a -q | xargs docker rm' ::: ${MARS_IP} ${PHOBOS_IP} ${DEIMOS_IP}
+parallel -v --linebuffer ssh root@{1} 'docker run ubuntu /bin/echo "Docker ready!"' ::: ${MARS_IP} ${PHOBOS_IP} ${DEIMOS_IP}
+parallel -v --linebuffer ssh root@{1} '"docker ps -a -q | xargs docker kill"' ::: ${MARS_IP} ${PHOBOS_IP} ${DEIMOS_IP}
+parallel -v --linebuffer ssh root@{1} '"docker ps -a -q | xargs docker rm"' ::: ${MARS_IP} ${PHOBOS_IP} ${DEIMOS_IP}
 # upload docker configuration to use TLS
 parallel -v --linebuffer scp -r etc/default/docker root@{1}:/etc/default/. ::: ${MARS_IP} ${PHOBOS_IP} ${DEIMOS_IP}
 ssh root@${MARS_IP} "cp -R ${MARS_CERTS} .docker"
@@ -86,28 +98,30 @@ ssh root@${MARS_IP} "service docker restart"
 ssh root@${PHOBOS_IP} "service docker restart"
 ssh root@${DEIMOS_IP} "service docker restart"
 
-# setup nginx on mars
-header "Install nginx"
-ssh root@${MARS_IP} "git clone https://github.com/JamesClonk/jcio-nginx-master"
-ssh root@${MARS_IP} "cd jcio-nginx-master; ./build.sh ${MARS}"
-ssh root@${MARS_IP} "docker run -d -p 80:80 -p 443:443 --name nginx-mars jcio-nginx-master"
-# TODO: set nginx to HTTPS reverse proxy, permanent redirect of http to HTTPS
 
 # setup shipyard on mars
 header "Install shipyard"
 ssh root@${MARS_IP} "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock shipyard/deploy start"
-# TODO: make sure nginx is setup as reverse proxy for HTTPS in front of shipyard
 # TODO: add new admin account to shipyard (use API over HTTPS!) (http://shipyard-project.com/docs/api/)
 # TODO: remove old admin account from shipyard (use API over HTTPS!) (http://shipyard-project.com/docs/api/)
 # TODO: add phobos and deimos as engines to shipyard (use API over HTTPS!) (http://shipyard-project.com/docs/api/)
 # TODO: add cpu- and memory-limit to "docker run" calls for containers.. for example 0.2 cpu, 64m for nginx?
-# TODO: give containers meaningful names when making "docker run" calls.. (--name)
+
 
 # setup haproxy and etcd on mars
 # TODO: setup haproxy and etcd on mars
 
+
 # setup nginx, frontend and backend on phobos and deimos
 # TODO: setup nginx, frontend and backend on phobos and deimos
+
+
+# setup nginx on mars (nginx must be last to run because it needs to link to other containers for reverse proxying)
+header "Install nginx"
+ssh root@${MARS_IP} "rm -rf jcio-nginx-master; git clone https://github.com/JamesClonk/jcio-nginx-master"
+ssh root@${MARS_IP} "cd jcio-nginx-master; ./build.sh ${MARS}"
+ssh root@${MARS_IP} "docker run -d -p 80:80 -p 443:443 --link shipyard:shipyard --name nginx-mars jcio-nginx-master"
+
 
 # cleanup
 header "It's cleanup time"
@@ -119,6 +133,7 @@ rm -vf droplets_to_provision.dat
 rm -vrf ${MARS_CERTS}
 rm -vrf ${PHOBOS_CERTS}
 rm -vrf ${DEIMOS_CERTS}
+
 
 echo ""
 success "All done!"
